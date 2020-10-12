9cbaadff3fdde40788c4cab8598490da1ff6352b
Diogo Vinicius <diogo.vinicius@gmail.com>
2020-10-12 14:56:24 -0300

    Obtendo um post por id

---
+++ src/main/java/com/springbootstudy/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.springbootstudy.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springbootstudy.workshopmongo.domain.Post;
+import com.springbootstudy.workshopmongo.domain.User;
+import com.springbootstudy.workshopmongo.repositories.PostRepository;
+import com.springbootstudy.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public List<Post> findAll() {
+		return postRepository.findAll();
+	}
+
+	public Post findById(String id) {
+		
+		Optional<Post> post = postRepository.findById(id);		
+		return post.orElseThrow(() -> new ObjectNotFoundException("User not found"));		
+	}
+}
