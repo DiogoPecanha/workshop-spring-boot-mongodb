51d348253869e45ecad6bfe67afea40ae314f37e
Diogo Vinicius <diogo.vinicius@gmail.com>
2020-10-12 11:27:34 -0300

    Exclusão de usuário com delete

--- src/main/java/com/springbootstudy/workshopmongo/services/UserService.java
+++ src/main/java/com/springbootstudy/workshopmongo/services/UserService.java
@@ -31,6 +31,11 @@ public class UserService {
 		return userRepository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
 	public User fromDto(UserDto userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
